37910a390b8ef4e8b292ac1062bc776a8243600b
andhikatk <roboticsarm98@gmail.com>
2022-08-22 19:53:56 +0700

    tes

--- scriptpertama.cs
+++ scriptpertama.cs
@@ -4,7 +4,7 @@ public class Helloword
 {
         public static void Main(string[]args)
         {
-         string jenengku = "Andhika Tri Cahyo Kumara";
+         string jenengku = "Andhika Tri CahyoKumara";
          string tinggaldi = "Rumah orang tua";
          int RT = 19 ;
          int RW =06;
