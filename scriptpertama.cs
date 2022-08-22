4f6ced8bc0b59136f05157ee4079598d9d4716d3
andhikatk <roboticsarm98@gmail.com>
2022-08-22 19:52:57 +0700

    first commit

---
+++ scriptpertama.cs
@@ -0,0 +1,22 @@
+//Source Code 005
+using System;
+public class Helloword
+{
+        public static void Main(string[]args)
+        {
+         string jenengku = "Andhika Tri Cahyo Kumara";
+         string tinggaldi = "Rumah orang tua";
+         int RT = 19 ;
+         int RW =06;
+         string sekolahdi= "SMKN 2 TRENGGALEK";
+         float jarak = 350.12f;
+         
+         Console.WriteLine("Nama : " + jenengku);
+         Console.WriteLine("Alamat : " + tinggaldi);
+         Console.WriteLine("RT: " + RT);
+         Console.WriteLine("RW: " + RW);
+         Console.WriteLine("Sekolah : " + sekolahdi);
+         Console.WriteLine("Jarak dari rumah kesekolah : " + jarak +"meter");
+
+        }
+}
